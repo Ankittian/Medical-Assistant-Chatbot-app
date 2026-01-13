1132d88a311c99050a4ad7f55dd9525d4b79a7e8
Ankittian <ankitrath58@gmail.com>
2026-01-13 14:54:57 +0530

    Folder structure added

---
+++ template.sh
@@ -0,0 +1,13 @@
+mkdir -p src
+mkdir -p research
+
+touch src/_init__.py
+touch src/helper.py
+touch src/prompt.py
+touch setup.py
+touch requirements.txt
+touch .env
+touch app.py 
+touch research/trials.ipynb
+
+echo "Project structure created successfully !"
